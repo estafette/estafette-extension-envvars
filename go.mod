0de69fb8cddfd65369d21735a2b3c8fac22ec8c4
Jorrit Salverda <jsalverda@travix.com>
2019-11-23 14:00:53 +0100

    use InitLoggingFromEnv

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/estafette/estafette-extension-envvars
+
+go 1.13
+
+require github.com/estafette/estafette-foundation v0.0.31
--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 	"runtime"
 	"sort"
 	"strings"
+
+	foundation "github.com/estafette/estafette-foundation"
 )
 
 var (
+	appgroup  string
+	app       string
 	version   string
 	branch    string
 	revision  string
@@ -18,12 +22,8 @@ var (
 
 func main() {
 
-	// log to stdout and hide timestamp
-	log.SetOutput(os.Stdout)
-	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
-
-	// log startup message
-	log.Printf("Starting estafette-extension-github-status version %v...\n\n", version)
+	// init log format from envvar ESTAFETTE_LOG_FORMAT
+	foundation.InitLoggingFromEnv(appgroup, app, version, branch, revision, buildDate)
 
 	log.Printf("All available estafette environment variables; the _DNS_SAFE suffixed ones can be used to set dns labels. Since leading digits are not allowed some of them are empty.\n\n")
 
