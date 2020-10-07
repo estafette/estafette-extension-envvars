92bc989e787f7b74ced00345a2ceba7c01f3bcb9
Jorrit Salverda <jsalverda@travix.com>
2020-10-07 15:30:32 +0200

    log some whitespace for improved readability

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func main() {
 	foundation.InitLoggingFromEnv(appgroup, app, version, branch, revision, buildDate)
 
 	log.Info().Msg("All available estafette environment variables; the _DNS_SAFE suffixed ones can be used to set dns labels. Since leading digits are not allowed some of them are empty.")
+	log.Info().Msg("")
 
 	estafetteEnvvars := []string{}
 
@@ -57,7 +58,9 @@ func main() {
 		}
 	}
 
+	log.Info().Msg("")
 	log.Info().Msg("All global envvars as defined in the manifest.")
+	log.Info().Msg("")
 
 	globalEnvvars := []string{}
 
